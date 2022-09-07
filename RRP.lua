ec745f504b11a5828da7bac531e4b9330dcec6f6
FriFrai <105680114+FriFrai@users.noreply.github.com>
2022-09-07 13:30:19 -0300

    Update RRP.lua

--- RRP.lua
+++ RRP.lua
@@ -81,7 +81,6 @@ if game.PlaceId == 9809240583 then
 						if v:IsA("TouchTransmitter") then
 							firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v.Parent, 0)
 							firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v.Parent, 1)
-						end
 					end
 				end
 			end
